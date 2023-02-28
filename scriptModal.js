e0755f02d2fe01eddd897cfd48af4ff7697edf41
npittigl <npittigl@gmail.com>
2023-02-28 12:08:34 -0500

    added modal to home page

---
+++ scriptModal.js
@@ -0,0 +1,12 @@
+// declare variables:
+const modalButton = document.querySelector('.btnClose');
+
+// add event listener to close button ('X')
+modalButton.addEventListener('click', function () {
+    // get target elements
+    const modal = document.querySelector('.modal');
+    const overlay = document.querySelector('.overlay');
+    // apply display: none to target elements when close button is clicked
+    modal.style.display = "none";
+    overlay.style.display = "none";
+});
